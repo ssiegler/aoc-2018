03ee163dedf17e0620ed681476b043dd4dbdda5f
Simon Siegler <simon.siegler@1und1.de>
2018-12-11 13:50:51 +0100

    Solve day 5 part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -3,12 +3,12 @@ extern crate aoc_2018;
 use aoc_2018::file_lines;
 
 fn main() {
-    let polymer = file_lines().collect();
-    let reduced = reduce(polymer);
-    println!("Remaining units: {}", reduced.len());
+    let polymer: String = file_lines().collect();
+    println!("Remaining units: {}", reduce(polymer.as_str()).len());
+    println!("Shortest polymer length: {}", find_shortest_variant_length(polymer.as_str()));
 }
 
-fn reduce(polymer: String) -> String {
+fn reduce(polymer: &str) -> String {
     let mut result = String::new();
     for unit in polymer.chars() {
         add_unit_with_reaction(&mut result, unit);
@@ -28,4 +28,14 @@ fn add_unit_with_reaction(result: &mut String, unit: char) {
 
 fn can_react(a: char, b: char) -> bool {
     a != b && a.eq_ignore_ascii_case(&b)
+}
+
+fn find_shortest_variant_length(polymer: &str) -> usize {
+    (b'a'..=b'z').map(char::from)
+        .map(|unit| reduce(remove_unit(polymer, &unit).as_str()).len())
+        .min().unwrap()
+}
+
+fn remove_unit(polymer: &str, unit: &char) -> String {
+    polymer.replace(|ch: char| ch.eq_ignore_ascii_case(unit), "")
 }
