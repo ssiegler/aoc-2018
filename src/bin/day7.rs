549e7000ede0ff1f595e7a8b3c5bcf0f11becab0
Simon Siegler <simon.siegler@gmail.com>
2018-12-21 17:13:34 +0100

    Simplify input handling

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,14 +1,19 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
+extern crate aoc_2018;
+
+use aoc_2018::file_lines;
 
 fn main() {
-
+    println!("Order: {}", order(file_lines()))
 }
 
-fn order(input: &str) -> String {
+fn order<I>(input: I) -> String
+where
+    I: Iterator,
+    I::Item: AsRef<str>,
+{
     let mut result = String::new();
-    for line in input.lines() {
-        let mut chars = line.chars();
+    for line in input {
+        let mut chars = line.as_ref().chars();
         if let Some(required) = chars.nth(5) {
             result.push(required);
         }
@@ -21,26 +26,28 @@ fn order(input: &str) -> String {
 
 #[cfg(test)]
 mod tests {
+    use std::iter::once;
+
     use super::*;
 
     #[test]
     fn no_tasks_without_dependencies() {
-        assert_eq!("", order(""));
+        assert_eq!("", order(once("")));
     }
 
     #[test]
     fn single_dependency() {
         assert_eq!(
             "AB",
-            order("Step A must be finished before step B can begin.")
+            order(once("Step A must be finished before step B can begin."))
         );
         assert_eq!(
             "CD",
-            order("Step C must be finished before step D can begin.")
+            order(once("Step C must be finished before step D can begin."))
         );
         assert_eq!(
             "CA",
-            order("Step C must be finished before step A can begin.")
+            order(once("Step C must be finished before step A can begin."))
         );
     }
 
@@ -49,8 +56,11 @@ mod tests {
         assert_eq!(
             "ABCD",
             order(
-                "Step A must be finished before step B can begin.
-Step C must be finished before step D can begin."
+                [
+                    "Step A must be finished before step B can begin.",
+                    "Step C must be finished before step D can begin."
+                ]
+                .iter()
             )
         );
     }
