3998b292b92fb09e41d8e00503a19413b018c113
Simon Siegler <simon.siegler@gmail.com>
2018-12-24 05:43:13 +0100

    Solve day 9 part 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 fn main() {
     println!("Score: {}", score(9, 25));
     println!("Score: {}", score(10, 1618));
@@ -6,14 +8,15 @@ fn main() {
     println!("Score: {}", score(21, 6111));
     println!("Score: {}", score(30, 5807));
     println!("Score: {}", score(428, 72061));
+    println!("Score: {}", score(428, 7_206_100));
 }
 
 fn score(players: usize, rounds: usize) -> usize {
     let mut scores: Vec<usize> = vec![0; players];
     let mut player = 0;
     let mut game = Game::new();
-    while game.next <= rounds {
-        if let Some(score) = game.turn() {
+    for round in 1..rounds {
+        if let Some(score) = game.add(round) {
             scores[player] += score;
         }
         player = (player + 1) % scores.len()
@@ -23,43 +26,42 @@ fn score(players: usize, rounds: usize) -> usize {
 
 #[derive(Debug)]
 struct Game {
-    marbles: Vec<usize>,
-    position: usize,
-    next: usize,
+    marbles: VecDeque<usize>,
 }
 
 impl Game {
     pub fn new() -> Self {
-        Game {
-            marbles: vec![0],
-            position: 1,
-            next: 1,
+        let mut game = Game {
+            marbles: VecDeque::new(),
+        };
+        game.marbles.push_front(0);
+        game
+    }
+
+    pub fn add(&mut self, marble: usize) -> Option<usize> {
+        if marble % 23 == 0 {
+            for _ in 0..7 {
+                self.move_counter_clockwise()
+            }
+            let result = self.marbles.pop_front().map(|removed| removed + marble);
+            result
+        } else {
+            self.move_clockwise();
+            self.move_clockwise();
+            self.marbles.push_front(marble);
+            None
         }
     }
 
-    pub fn turn(&mut self) -> Option<usize> {
-        let result = if self.next % 23 != 0 {
-            if self.position < self.marbles.len() {
-                self.marbles.insert(self.position + 1, self.next);
-                self.position += 2
-            } else {
-                self.marbles.insert(1, self.next);
-                self.position = 2;
-            }
-            None
-        } else {
-            if self.position > 7 {
-                self.position -= 7;
-            } else if self.marbles.len() > 7 {
-                self.position = self.marbles.len() - 7 + self.position;
-            } else {
-                unimplemented!()
-            }
-            let score = self.next + self.marbles.remove(self.position - 1);
-            Some(score)
-        };
-        self.next += 1;
-        result
+    fn move_clockwise(&mut self) {
+        if let Some(current) = self.marbles.pop_front() {
+            self.marbles.push_back(current);
+        }
+    }
+
+    fn move_counter_clockwise(&mut self) {
+        if let Some(counter_clock_wise) = self.marbles.pop_back() {
+            self.marbles.push_front(counter_clock_wise);
+        }
     }
 }
-
